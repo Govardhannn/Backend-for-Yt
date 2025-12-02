489b0ad2755901cf0c8742cffbf3f4816f5bb1c7
Govardhannn <sahanigovardhan69@gmail.com>
2025-12-02 09:36:50 +0000

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,40 @@
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+dotenv.config();
+
+connectDB()
+
+
+
+
+
+
+
+// this is fot the biggner or Intern 
+/*
+import express from "express";
+
+const app = express()
+
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
+    {
+      DB_NAME;
+    }
+    app.on("error", () => {
+      console.log("ERROR:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listeing in port 
+                ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR:", error);
+  }
+})();
+*/
